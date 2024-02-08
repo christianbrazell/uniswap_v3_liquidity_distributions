73a4cc0bae8576fd324fcedfed2f984c8f7b2ad7
christianbrazell <christiantbrazell@gmail.com>
2024-02-08 11:31:58 -0600

    Reformatted and cleaned

--- get_distribution.py
+++ get_distribution.py
@@ -1,30 +1,34 @@
-import math
 import os
+import argparse
 
 from utils.subgraph import *
 from utils.uniswap import *
 
+url = "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3?source=uniswap" # Subgraph URL
+default_pool_address = "0x88e6a0c2ddd26feeb64f039a2c41296fcb3f5640"  # Default USDC/ETH 0.05%
+default_surrounding_ticks = 300  # How many ticks on either side of active tick to query
 
-url = "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3?source=uniswap"
+# Load subgraph queries
+pool_query_path = os.path.join('subgraph_queries', 'pool_query.txt')
+tick_query_path = os.path.join('subgraph_queries', 'tick_query.txt')
 
-pool_address = "0x88e6a0c2ddd26feeb64f039a2c41296fcb3f5640"
-price_fixed_digits = 4
-default_surround_ticks = 300
+with open(pool_query_path, 'r') as file:
+    pool_query = file.read()
 
-num_surround_ticks = default_surround_ticks
+with open(tick_query_path, 'r') as file:
+    tick_query = file.read()
 
+if __name__ == "__main__":
 
-if __name__=="__main__":
+    # Set query parameters from command line
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('--pool_id', type=str, help='ID of the pool', default=default_pool_address)
+    parser.add_argument('--surround_ticks', type=int, help='Number of surround ticks',
+                        default=default_surrounding_ticks)
 
-    # Load subgraph queries
-    pool_query_path = os.path.join('subgraph_queries', 'pool_query.txt')
-    tick_query_path = os.path.join('subgraph_queries', 'tick_query.txt')
-
-    with open(pool_query_path, 'r') as file:
-        pool_query = file.read()
-
-    with open(tick_query_path, 'r') as file:
-        tick_query = file.read()
+    args = parser.parse_args()
+    pool_address = args.pool_id
+    surrounding_ticks = default_surrounding_ticks
 
     # Get pool data
     pool_data_payload = {
@@ -39,8 +43,8 @@ if __name__=="__main__":
     pool = Pool(pool_data)
 
     # Get lower and upper tick bounds for analysis
-    tick_lower_bound = pool.active_tick_lower - pool.tick_spacing * num_surround_ticks
-    tick_upper_bound = pool.active_tick_lower + pool.tick_spacing * num_surround_ticks
+    tick_lower_bound = pool.active_tick_lower - pool.tick_spacing * surrounding_ticks
+    tick_upper_bound = pool.active_tick_lower + pool.tick_spacing * surrounding_ticks
 
     # Get tick data
     tick_data_payload = {
@@ -60,17 +64,22 @@ if __name__=="__main__":
     tick_df = pd.DataFrame(tick_data)
     for col in tick_df.columns:
         tick_df[col] = pd.to_numeric(tick_df[col], errors='coerce', downcast='float')
-    tick_df['price'] = tick_to_adjusted_price(tick_df['tickIdx'], pool.token_0, pool.token_1)
     tick_df['cumulative_liquidityNet'] = tick_df['liquidityNet'].cumsum()
 
-    active_tick_df = tick_df.loc[tick_df.tickIdx==pool.active_tick_lower]
+    # Calculate liquidity at each tick
+    # First, find the difference between liquidity and cumulative liquidityNet at active tick
+    active_tick_df = tick_df.loc[tick_df.tickIdx == pool.active_tick_lower]
     liquidity_offset = pool.liquidity - active_tick_df.cumulative_liquidityNet.values[0]
+
+    # Second, add this offset to all ticks to get their liquidity
     tick_df['liquidity'] = tick_df.cumulative_liquidityNet + liquidity_offset
 
     # Convert liquidity into real token amounts
+    # First, set the lower and upper ticks for the liquidity ranges
     tick_df['tick_upper'] = tick_df.tickIdx
     tick_df['tick_lower'] = tick_df.tickIdx - pool.tick_spacing
 
+    # Second, process the LP amounts tick by tick
     token_amount_0_list = []
     token_amount_1_list = []
     for (index, row) in tick_df.iterrows():
@@ -81,4 +90,7 @@ if __name__=="__main__":
     tick_df['amount_0'] = token_amount_0_list
     tick_df['amount_1'] = token_amount_1_list
 
+    # Include price at each tick, for reference
+    tick_df['price'] = tick_to_adjusted_price(tick_df['tickIdx'], pool.token_0, pool.token_1)
 
+    lp_df = tick_df[['price', 'amount_0', 'amount_1']]
--- utils/uniswap.py
+++ utils/uniswap.py
@@ -1,6 +1,7 @@
 import math
 import pandas as pd
 
+
 def fee_tier_to_tick_spacing(fee_tier):
     return {
         '10000': 200,
@@ -9,76 +10,16 @@ def fee_tier_to_tick_spacing(fee_tier):
         '100': 1
     }.get(fee_tier, 60)
 
+
 def tick_to_adjusted_price(tick, token_0, token_1):
     return 1. / ((1.0001 ** tick) / 10 ** (token_1.decimals - token_0.decimals))
+
 
 def tick_to_price(tick):
     return 1.0001 ** tick
 
 
-# def process_liquidity(ticks, pool):
-#
-#     # # Get current tick amounts
-#     # price = tick_to_price(pool.tick)
-#     # price_a = tick_to_price(pool.active_tick_lower)
-#     # price_b = tick_to_price(pool.active_tick_upper)
-#     #
-#     # sp = price ** 0.5
-#     # sa = price_a ** 0.5
-#     # sb = price_b ** 0.5
-#     #
-#     # active_amount_0 = pool.liquidity * (sb - sp) / (sp * sb)
-#     # active_amount_1 = pool.liquidity * (sp - sa)
-#     #
-#     # # Adjust to human-readable format
-#     # adjusted_active_amount_0 = active_amount_0 / 10 ** pool.token_0.decimals
-#     # adjusted_active_amount_1 = active_amount_1 / 10 ** pool.token_1.decimals
-#
-#     # Get current tick liquidity
-#     liquidity_active = pd.DataFrame(
-#         {
-#             "tick": [pool.active_tick_lower],
-#             "liquidity": [pool.liquidity]
-#         }
-#     )
-#
-#     # Get top half liquidity
-#     ticks_above = ticks.loc[ticks.tickIdx > pool.active_tick_upper].sort_values(by="tickIdx", ascending=True)
-#     liquidity = pool.liquidity
-#     liquidity_above = pd.DataFrame(
-#         {
-#             "tick": [],
-#             "liquidity": []
-#         }
-#     )
-#     for (index, row) in ticks_above.iterrows():
-#         liquidity += row.liquidityNet
-#         liquidity_above.loc[len(liquidity_above)] = [row.tickIdx, liquidity]
-#
-#     # Get bottom half liquidity
-#     ticks_below = ticks.loc[ticks.tickIdx < pool.active_tick_lower].sort_values(by="tickIdx", ascending=False)
-#     liquidity = pool.liquidity
-#     liquidity_below = pd.DataFrame(
-#         {
-#             "tick": [],
-#             "liquidity": []
-#         }
-#     )
-#     for (index, row) in ticks_below.iterrows():
-#         liquidity += row.liquidityNet
-#         liquidity_below.loc[len(liquidity_below)] = [row.tickIdx, liquidity]
-#
-#     # Join dataframes
-#     liquidity_df = pd.concat(
-#         [liquidity_below, liquidity_active, liquidity_above]
-#     ).sort_values(by="tick", ascending=True)
-#
-#
-#     return liquidity_df
-
-
 def process_tick(tick, pool):
-
     tick_lower = tick.tick_lower
     tick_upper = tick.tick_upper
     liquidity = tick.liquidity
@@ -115,7 +56,6 @@ def process_tick(tick, pool):
     else:
         amount_0 = 0.00
         amount_1 = 0.00
-        pass
 
     # Adjust to human-readable format
     adjusted_amount_0 = amount_0 / 10 ** pool.token_0.decimals
@@ -123,11 +63,13 @@ def process_tick(tick, pool):
 
     return adjusted_amount_0, adjusted_amount_1
 
+
 class Token:
     def __init__(self, token):
         self.symbol = token['symbol']
         self.decimals = int(token['decimals'])
         self.id = token['id']
+
 
 class Pool:
     def __init__(self, pool):
@@ -136,9 +78,7 @@ class Pool:
         self.tick = int(pool['tick'])
         self.fee_tier = pool['feeTier']
         self.liquidity = int(pool['liquidity'])
-        # self.tvl = float(pool['totalValueLockedUSD'])
 
         self.tick_spacing = fee_tier_to_tick_spacing(self.fee_tier)
         self.active_tick_lower = math.floor(self.tick / self.tick_spacing) * self.tick_spacing
         self.active_tick_upper = self.active_tick_lower + self.tick_spacing
-
